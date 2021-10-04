0c643104de6218612e22c4630330664c1dbf7f72
Allan Winckler <awmoreira@gmail.com>
2021-10-04 12:24:27 -0300

    feat: screen signup

--- src/screens/SignUp/index.tsx
+++ src/screens/SignUp/index.tsx
@@ -1,7 +1,49 @@
 import React from 'react';
 
-import {Container} from './styles';
+import Input from '~/components/TextInput';
+import {PrimaryButton} from '~/components/Button';
+import {Email, Padlock, User} from '~/assets/svgs';
 
-const SignUp: React.FC = () => <Container />;
+import {
+  Container,
+  WrapperTop,
+  WrapperCenter,
+  WrapperBottom,
+  Title,
+  Description,
+  DescriptionOrange,
+  WrapperInputs,
+} from './styles';
+
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <WrapperTop />
+      <WrapperCenter>
+        <Title>Criar Conta</Title>
+        <WrapperInputs>
+          <Input placeholder="Nome completo" icon={<User color="#748C94" />} />
+          <Input placeholder="Email" icon={<Email color="#748C94" />} />
+
+          <Input
+            placeholder="Senha"
+            icon={<Padlock color="#748C94" />}
+            secureTextEntry
+          />
+          <Input
+            placeholder="Confirmar Senha"
+            icon={<Padlock color="#748C94" />}
+            secureTextEntry
+          />
+        </WrapperInputs>
+        <PrimaryButton>Cadastrar</PrimaryButton>
+      </WrapperCenter>
+      <WrapperBottom>
+        <Description>Já tem uma conta? </Description>
+        <DescriptionOrange>Faça o login</DescriptionOrange>
+      </WrapperBottom>
+    </Container>
+  );
+};
 
 export default SignUp;
--- src/screens/SignUp/styles.ts
+++ src/screens/SignUp/styles.ts
@@ -1,3 +1,53 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({theme}) => theme.colors.white};
+`;
+
+export const WrapperTop = styled.View`
+  flex: 1;
+`;
+export const WrapperCenter = styled.View`
+  flex: 2;
+  padding-horizontal: 21px;
+`;
+export const WrapperBottom = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-end;
+  padding-bottom: 38px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({theme}) => theme.fonts.inter.regular};
+  font-weight: 600;
+  font-size: 25px;
+
+  color: ${({theme}) => theme.colors.blackBlue};
+`;
+
+export const Description = styled.Text`
+  font-family: ${({theme}) => theme.fonts.karla.regular};
+  font-weight: 400;
+  font-size: 15px;
+
+  color: ${({theme}) => theme.colors.text};
+`;
+
+export const DescriptionOrange = styled.Text`
+  font-family: ${({theme}) => theme.fonts.karla.bold};
+  font-weight: 700;
+  font-size: 15px;
+
+  color: ${({theme}) => theme.colors.orange};
+`;
+
+export const WrapperInputs = styled.View`
+  min-height: 125px;
+  margin-top: 22px;
+  margin-bottom: 30px;
+  flex: 1;
+  justify-content: space-between;
+`;
